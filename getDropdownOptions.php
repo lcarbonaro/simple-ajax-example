c815e5d532f1a26875fe436304ceaf50aa0ec6c3
lcarbonaro <lcarbonaro@gmail.com>
2014-11-30 20:02:10 +0000

    initial commit

---
+++ getDropdownOptions.php
@@ -0,0 +1,47 @@
+<?php
+
+// This PHP script fetches values from a database table,
+// formats these values into HTML option tags, and 
+// passes the result back.
+// This result will then be used to populate the drop-down.
+
+// 1. establish database connection
+$dbHost = '0.0.0.0';
+$dbUser = 'lcarbonaro';
+$dbPass = '';
+$dbName = 'test';
+$dbConn = mysqli_connect($dbHost, $dbUser, $dbPass, $dbName);
+if ( !$dbConn ) {
+    exit('Database Connection Error (' . mysqli_connect_errno() . ') ' . mysqli_connect_error());
+}
+
+// 2. query out the data
+$sqlStatement = "SELECT `id`,`method_name` FROM `paymethod`";
+$sqlResult    = mysqli_query($dbConn, $sqlStatement);
+if ( !$sqlResult ) {
+    exit('MySQL Error: ' . mysqli_error($dbConn) . 'SQL: ' . $sqlStatement);
+}
+
+// 3. prepare the data for presentation i.e. format values as HTML option tags
+$options = '';
+while ( $row = mysqli_fetch_assoc($sqlResult) ) {
+    $options .= '<option value="'.$row['id'].'">'.$row['method_name'].'</option>';
+}
+
+// 4. return prepared data
+echo($options);
+
+/*
+-- The code shown above assumes this table structure:
+CREATE TABLE `paymethod` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `method_name` varchar(25),
+  PRIMARY KEY (`id`)
+);
+INSERT INTO `paymethod` (`id`, `method_name`) VALUES
+(1, 'Credit Card'),
+(2, 'Debit Card'),
+(3, 'Cash'),
+(4, 'Cheque');
+
+*/
---
+++ script.js
@@ -0,0 +1,33 @@
+/* 
+ * This script does the AJAX call to the PHP script
+ * which returns the data for the drop-down, i.e. the 
+ * option tags with data from the database table.
+ * 
+ * Ref: https://developer.mozilla.org/en-US/docs/Web/API/XMLHttpRequest
+ * 
+ */
+
+function doAjaxCall() {
+    
+    // 1. create an XMLHttpRequest object
+    var xhr = new XMLHttpRequest();
+    
+    // 2. set up an event handler, i.e. what to do once the request is sent
+    // note that this does not execute until after step 3
+    xhr.onreadystatechange = function() {
+        // readyState 4 means DONE (request complete)
+        // status 200 is the usual HTTP code for success
+        if ( xhr.readyState===4 && xhr.status===200 ) {  
+            // prepared data from the PHP script is returned here as responseText
+            // "inject" this responseText into <select> element (the dropdown)
+            document.getElementById("mydropdown").innerHTML = xhr.responseText;
+        }
+    };
+    
+    // 3. initialise a request and send it
+    xhr.open("GET", "getDropdownOptions.php", true);  // last parm (true) refers to asynchronous
+    xhr.send();  
+    
+}
+
+
